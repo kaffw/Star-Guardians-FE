61b337238de664f72d65be715c7a55517decc206
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-11-21 00:41:17 +0800

    UPDATE (Game Manager)
    Character Manager have state values that manages human movement, and manananngal movement including separated movement of manananggal upper body, and manananggal lower body.

--- Star Guardians/Assets/Scripts/CharacterManager.cs
+++ Star Guardians/Assets/Scripts/CharacterManager.cs
@@ -5,43 +5,84 @@ using UnityEngine.UI;
 
 public class CharacterManager : MonoBehaviour
 {
-    public Sprite[] bodySprite;
-    public bool isNight = false;
+    //profile image for active body
+    public Sprite[] profileSprite;
     public Image profileImage;
 
-    public MonoBehaviour upperBodyMovement, lowerBodyMovement;
-    public GameObject upperBodyCam, lowerBodyCam;
+    public bool isNight = false; // get val from Day/Night Manager (currently on redo progress)
 
+    //Camera and Movement Manager
+    public MonoBehaviour upperBodyMovement, lowerBodyMovement;
+    public GameObject upperBodyCam, lowerBodyCam, humanBodyCam;
+
+    public enum BodyState { Human, Manananggal };
+    public BodyState state;
+    public bool isDetached = false; //for manananggal state
     void Update()
     {
         //enable if night
 
-        if (Input.GetKeyDown(KeyCode.M)) isNight = false;
+        if (Input.GetKeyDown(KeyCode.M)) isNight = false; //temp day/ night setter
         else if(Input.GetKeyDown(KeyCode.N)) isNight = true;
 
-        if(isNight)
+        if (isNight)
         {
-            if(Input.GetKeyDown(KeyCode.Alpha1))
+            state = BodyState.Manananggal;
+            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                //enable movement for manananggal, disable lower body movement
-                profileImage.sprite = bodySprite[0];
-                upperBodyMovement.enabled = true;
-                lowerBodyMovement.enabled = false;
-
-                upperBodyCam.SetActive(true);
-                lowerBodyCam.SetActive(false);
+                UpperBodyMode();
             }
-            else if(Input.GetKeyDown(KeyCode.Alpha2))
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                //enable movement for lower body, disable movement for manananggal
-                profileImage.sprite = bodySprite[1];
-                lowerBodyMovement.enabled = true;
-                upperBodyMovement.enabled = false;
-
-                upperBodyCam.SetActive(false);
-                lowerBodyCam.SetActive(true);
+                LowerBodyMode();
             }
         }
-        
+        else
+        {
+            state = BodyState.Human;
+            humanBodyCam.SetActive(true);
+        }
+
+        //Debug.Log(state);
+    }
+
+    void UpperBodyMode()
+    {
+        //enable movement for manananggal, disable lower body movement
+        profileImage.sprite = profileSprite[0];
+        upperBodyMovement.enabled = true;
+        lowerBodyMovement.enabled = false;
+
+        upperBodyCam.SetActive(true);
+        lowerBodyCam.SetActive(false);
+        humanBodyCam.SetActive(false);
+    }
+
+    void LowerBodyMode()
+    {
+        //enable movement for lower body, disable movement for manananggal
+        profileImage.sprite = profileSprite[1];
+        lowerBodyMovement.enabled = true;
+        upperBodyMovement.enabled = false;
+
+        upperBodyCam.SetActive(false);
+        lowerBodyCam.SetActive(true);
+        humanBodyCam.SetActive(false);
+    }
+
+    public void AttachBody()
+    {
+        //Check if mUB is within mLB, keybind, call AttachBody.
+    }
+
+    public void DetachBody()
+    {
+        //Check if mUB is within mLB, keybind, call AttachBody.
     }
 }
+/*
+Terms
+h       :   Human
+mUB     :   Manananggal Upper Body
+mLB     :   Manananggal Lower Body
+ */
