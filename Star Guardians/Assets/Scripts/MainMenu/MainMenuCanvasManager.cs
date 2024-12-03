c4d2fc31b9c36d5c3749824a86205a3827778114
John Michael Coronel <139611817+kaffw@users.noreply.github.com>
2024-12-04 00:38:09 +0800

    UPDATE (Game Main Menu)
    Update functionalities
    - Enters and Exit Options, Credits
    - Start Game
    - Exit Game

--- Star Guardians/Assets/Scripts/MainMenu/MainMenuCanvasManager.cs
+++ Star Guardians/Assets/Scripts/MainMenu/MainMenuCanvasManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class MainMenuCanvasManager : MonoBehaviour
 {
+    [Header("Splash Art")]
     public Image splashArt;
     public Sprite[] splashArtSprite;
     private int splashIndex;
 
     public float timer;
+
+    //-------------------------------//
+    [Header("Canvases")]
+    public GameObject mainMenuCanvas;
+    public GameObject optionsCanvas;
+    public GameObject creditsCanvas;
+
     void Start()
     {
         timer = 0f;
@@ -19,17 +27,49 @@ public class MainMenuCanvasManager : MonoBehaviour
 
     void Update()
     {
+        UpdateSplash();
+    }
+    public void PlayGame()
+    {
+        Debug.Log("Plays Game");
+        SceneManager.LoadScene(1);
+    }
+
+    public void EnterOptions()
+    {
+        optionsCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+
+    public void EnterCredits()
+    {
+        optionsCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+
+    }
+
+    public void BackToMainMenu()
+    {
+        optionsCanvas.SetActive(false);
+        creditsCanvas.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    void UpdateSplash()
+    {
         timer += Time.deltaTime;
 
         if (timer >= 60)
         {
             timer = 0f;
             splashIndex = (splashIndex == 0) ? 1 : 0;
-            StartCoroutine(UpdateSplash());
+            StartCoroutine(UpdateSplashSprite());
         }
     }
 
-    IEnumerator UpdateSplash()
+    IEnumerator UpdateSplashSprite()
     {
         for (float alpha = 1f; alpha >= 0f; alpha -= Time.deltaTime)
         {
